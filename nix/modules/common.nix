270c11eb7f6684a7074bac29bd804b6f07667e47
zeroCountersteer <fireboll.tv@gmail.com>
2025-08-12 08:58:46 +0500

    upd

--- nix/modules/common.nix
+++ nix/modules/common.nix
@@ -9,7 +9,27 @@
   nix.optimise.automatic = true;
 
   time.timeZone = "Asia/Almaty";
+
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_NUMERIC = "en_IE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
+    LC_PAPER = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
+    LC_ADDRESS = "en_IE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_TELEPHONE = "en_IE.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_COLLATE = "en_US.UTF-8";
+  };
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "en_IE.UTF-8/UTF-8"
+  ];
+
 
   networking.networkmanager.enable = true;
 
