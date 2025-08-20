6828c7da7a6e550e175f4d1e01f6466349c951e1
zeroCountersteer <fireboll.tv@gmail.com>
2025-08-20 23:40:26 +0500

    Update common.nix

--- nix/modules/common.nix
+++ nix/modules/common.nix
@@ -1,99 +1,132 @@
 { config, pkgs, lib, ... }:
 let
   wall = ../../assets/wallpapers/02.jpg;
-in
-{
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = false;
+in {
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    grub.enable = false;
+  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.gc = { automatic = true; dates = "weekly"; options = "--delete-older-than 14d"; };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
   nix.optimise.automatic = true;
 
   time.timeZone = "Asia/Almaty";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_NUMERIC = "en_IE.UTF-8";
-    LC_TIME = "en_IE.UTF-8";
-    LC_MONETARY = "en_IE.UTF-8";
-    LC_PAPER = "en_IE.UTF-8";
-    LC_MEASUREMENT = "en_IE.UTF-8";
-    LC_ADDRESS = "en_IE.UTF-8";
-    LC_IDENTIFICATION = "en_IE.UTF-8";
-    LC_TELEPHONE = "en_IE.UTF-8";
-    LC_NAME = "en_IE.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    LC_MESSAGES = "en_US.UTF-8";
-    LC_COLLATE = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_NUMERIC = "en_IE.UTF-8";
+      LC_TIME = "en_IE.UTF-8";
+      LC_MONETARY = "en_IE.UTF-8";
+      LC_PAPER = "en_IE.UTF-8";
+      LC_MEASUREMENT = "en_IE.UTF-8";
+      LC_ADDRESS = "en_IE.UTF-8";
+      LC_IDENTIFICATION = "en_IE.UTF-8";
+      LC_TELEPHONE = "en_IE.UTF-8";
+      LC_NAME = "en_IE.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_COLLATE = "en_US.UTF-8";
+    };
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "en_IE.UTF-8/UTF-8"
+    ];
   };
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "en_IE.UTF-8/UTF-8"
-  ];
 
-  networking.networkmanager.enable = true;
+  systemd.network.enable = false;
+  networking.wireless.enable = lib.mkForce false;
 
-  services.xserver.enable = true;
-  services.xserver.xkb.layout = "us";
-
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+  networking.networkmanager = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    ensureProfiles = {
+      home = {
+        connection.id = "home";
+        connection.type = "wifi";
+        wifi.ssid = "CXNK0068CF2C";
+        wifi.mode = "infrastructure";
+        wifi-security.key-mgmt = "wpa-psk";
+        wifi-security.psk = "e6ca58bf654c9f44";
+        ipv4.method = "auto";
+        ipv6.method = "auto";
+      };
+    };
   };
 
-  users.users.yuki = {
-    isNormalUser = true;
-    description = "yuki";
-    extraGroups = [ "networkmanager" "wheel" "dialout" ];
+  services = {
+    xserver.enable = false;
+    printing.enable = true;
+
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    dbus.enable = true;
+    greetd = {
+      enable = true;
+      settings = {
+        terminal.vt = 1;
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
+          user = "greeter";
+        };
+      };
+    };
   };
-  users.defaultUserShell = pkgs.nushell;
+
+  security = {
+    rtkit.enable = true;
+    polkit.enable = true;
+  };
+
+  users = {
+    defaultUserShell = pkgs.nushell;
+    users.yuki = {
+      isNormalUser = true;
+      description = "yuki";
+      extraGroups = [
+        "wheel" "networkmanager" "dialout"
+        "video" "render" "input" "audio"
+      ];
+    };
+  };
 
   nixpkgs.config.allowUnfree = true;
 
-  hardware.graphics.enable = true;
-  hardware.graphics.enable32Bit = true;
-
-  services.displayManager.defaultSession = "plasmax11";
-
-  services.xserver.displayManager.lightdm = {
+  hardware.graphics = {
     enable = true;
-    greeters.slick.enable = true;
-    greeters.slick.extraConfig = ''
-      background=${builtins.toString wall}
-      logind-check-graphical=true
-    '';
+    enable32Bit = true;
   };
 
   programs.hyprland.enable = true;
 
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-  services.desktopManager.plasma6.enable = true;
-
-  environment.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";
-    NIXOS_OZONE_WL = "1";
-    QT_QPA_PLATFORM = "wayland";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    XDG_SESSION_TYPE = "wayland";
-    XCURSOR_SIZE = "24";
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+    config.common.default = [ "hyprland" "gtk" ];
   };
 
-  environment.systemPackages = with pkgs; [
-    git
-  ];
-
-  home-manager.users.yuki = {
-    programs.plasma.enable = true;
-    programs.plasma.workspace.wallpaper = wall;
+  environment = {
+    sessionVariables = {
+      MOZ_ENABLE_WAYLAND = "1";
+      NIXOS_OZONE_WL = "1";
+      QT_QPA_PLATFORM = "wayland";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      XCURSOR_SIZE = "24";
+    };
+    systemPackages = with pkgs; [ git ];
   };
 }
